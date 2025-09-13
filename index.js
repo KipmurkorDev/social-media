0d65aad73653ff65b252e60ab980fbcc7b48492c
KipmurkorDev <emmanuelkipmurkor@gmail.com>
2025-09-13 13:47:22 +0300

    fixes

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import { runTwitterAgent } from "./platforms/twitter.js";
+import { runTwitterAgent } from "./platforms/twitter";
+
 (async () => {
-  await runTwitterAgent();
+  try {
+    await runTwitterAgent();
+    console.log("✅ Tweet posted successfully. Exiting...");
+    process.exit(0);
+  } catch (error) {
+    console.error("❌ Error running Twitter agent:", error);
+    process.exit(1);
+  }
 })();
